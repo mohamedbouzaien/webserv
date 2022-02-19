3b86a9d0e1509f2ba07dad57e7bf44b5fb0a044c
Loic Fournerat <lfourner@student.42.fr>
2022-02-19 14:29:12 +0100

    planifying multi_port, think i figured out how to actually do it

--- classes/Poller.cpp
+++ classes/Poller.cpp
@@ -46,7 +46,9 @@ void        Poller::start(void)
 {
 	int	rc;
 
+    std::cout << "ui\n";
 	rc = poll(_fds, _nfds, -1);
+    std::cout << "ui\n";
 	if (rc < 0)
 		throw	Poller::PollFailedException();
 }
@@ -59,27 +61,32 @@ void        Poller::handle(const Server_t &serv_conf)
 	current_sockets = _nfds;
 	for (int i = 0; i < current_sockets; i++)
 	{
+        std::cout << _fds[i].fd << " " << _fds[i].revents << "\n" ;
 		if(_fds[i].revents == 0)
 			continue;
-		if (_fds[i].fd == _listener.getFd())
+		if (_fds[i].fd == _listener.getFd())  //TODO comparer avec listener collec
 		{
+            //TODO connector init(celui quon a trouve au dessus)
 			std::cout << "  Listening socket is readable" << std::endl;
 			connector.accept_c();
 			std::cout << "  New incoming connection - " << connector.getClientSocket() << std::endl;
 			_fds[_nfds].fd = connector.getClientSocket();
 			_fds[_nfds].events = POLLIN | POLLPRI;
-			_nfds++;
-			}
+			++_nfds;
+            //TODO map <int index, Listener lstn>
+        }
 		else
 		{
 			std::cout << "  Descriptor " << _fds[i].fd << " is readable" << std::endl;
+            //TODO Connector(client_map[i]);
 			connector.setClientSocket(_fds[i].fd);
 			if (connector.handle(serv_conf) < 0)
 			{
 				_fds[i].fd = 0;
 				_fds[i].events = 0;
 				_fds[i].revents = 0;
-				_nfds--;
+                //TODO close () ??
+				--_nfds;
 			}
 		}
 	}
